cdd5fc6422ce8159d1312eda4fb333d7dd910025
Efraín Córdova <52300931+ecordova0975@users.noreply.github.com>
2020-10-29 15:31:44 -0500

    VIZCARRRRA

---
+++ transition.h
@@ -0,0 +1,24 @@
+#ifndef TEO_PROYECTO_TRANSITION_H
+#define TEO_PROYECTO_TRANSITION_H
+#include "automata.h"
+
+// Alfabeto = {0,1,-1 (epsilon)}
+struct Transition{
+    Transition(int data, State* id1, State* id2);
+    State* states[2] {};
+    int data {};
+    ~Transition();
+};
+
+Transition::Transition(int data, State *id1, State *id2) {
+    this->data = data;
+    this->states[0] = id1;
+    this->states[1] = id2;
+}
+
+Transition::~Transition() {
+   for(auto state: states)
+       state = nullptr;
+}
+
+#endif //TEO_PROYECTO_TRANSITION_H
