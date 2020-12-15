1939362ed6932b99f7f110babd565a66d7c51e3e
IWeseI <57014490+IWeseI@users.noreply.github.com>
2020-12-15 16:00:39 -0500

    RESOLVE PROBLEMS

--- bettermatrix.h
+++ bettermatrix.h
@@ -3,8 +3,8 @@
 #include "lib.h"
 #include "detautomata.h"
 #include "equivalencia.h"
-//ABCDE -> AB AC AD BC BD CD
-void dfs_recursivo(Automata a, State* first_state, State* second_state, vector<pair<int,int>>& pairVector, bool& distingishable);
+
+void dfs_recursivo(Automata a, State* first_state, State* second_state, vector<pair<int,int>>& pairVector,vector<pair<int,int>>& visited, bool& distingishable);
 pair<vector<pair<int,int>>, bool> dfs(Automata a, int first, int second);
 
 vector<vector<bool>> equivalencia_optimizada(const Automata& a, vector<vector<bool>> xd){
@@ -25,69 +25,54 @@ vector<vector<bool>> equivalencia_optimizada(const Automata& a, vector<vector<bo
         }
         it++;
     }
-    set<pair<int,int>> visited {};
+    vector<pair<int,int>> visited {};
     for(auto pairIt: pairVector){
         int first = pairIt.first;
         int second = pairIt.second;
-        if(visited.find(pairIt) == visited.end()) {
+        if(find(visited.begin(), visited.end(), pairIt) == visited.end()) {
             auto pairOut = dfs(a, first, second);
             for (auto innerPairIt: pairOut.first) {
+                visited.emplace_back(innerPairIt);
                 out[innerPairIt.first][innerPairIt.second] = !pairOut.second;
                 out[innerPairIt.second][innerPairIt.first] = !pairOut.second;
-                cout<<endl;
             }
         }
     }
+
     return out;
 };
 
 pair<vector<pair<int,int>>, bool> dfs(Automata a, int first, int second){
-    auto first_state = a.states[first];
-    auto second_state = a.states[second];
-    Transition* zero_first_transition;
-    Transition* one_first_transition;
-    Transition* zero_second_transition;
-    Transition* one_second_transition;
-    auto transition = first_state->transitions[0];
-    switch(transition->data) {
-        case 0:
-            zero_first_transition = transition;
-            one_first_transition = first_state->transitions[1];
-            break;
-        case 1:
-            one_first_transition = transition;
-            zero_first_transition = first_state->transitions[1];
-            break;
-        default: throw out_of_range("ERROR: El Automata no es un AFD");
-    }
-    transition = second_state->transitions[1];
-    switch(transition->data) {
-        case 0:
-            zero_second_transition = transition;
-            one_second_transition = first_state->transitions[1];
-            break;
-        case 1:
-            one_second_transition = transition;
-            zero_second_transition = first_state->transitions[1];
-            break;
-        default: throw out_of_range("ERROR: El Automata no es un AFD");
-    }
-    vector<pair<int,int>> outVector {};
+    vector<pair<int,int>> outVector;
+    vector<pair<int,int>> visited;
     bool distingishable = false;
-    dfs_recursivo(a, zero_first_transition->states[1], zero_second_transition->states[1], outVector, distingishable);
-    dfs_recursivo(a, one_first_transition->states[1], one_second_transition->states[1], outVector, distingishable);
-    return make_pair(outVector, distingishable);
+    dfs_recursivo(a, a.states[first], a.states[second], outVector, visited, distingishable);
+    if(distingishable){
+        return make_pair(outVector, distingishable);
+    }
+    return make_pair(visited, distingishable);
 }
 
-void dfs_recursivo(Automata a, State* first_state, State* second_state, vector<pair<int,int>>& pairVector, bool& distingishable){
-    bool c1 = find(a.finalStates.begin(), a.finalStates.end(), first_state->id) == a.finalStates.end();
-    bool c2 = find(a.finalStates.begin(), a.finalStates.end(), second_state->id) == a.finalStates.end();
-    bool c3 = find(pairVector.begin(), pairVector.end(), make_pair(first_state->id, second_state->id)) != pairVector.end();
-    bool c4 = find(pairVector.begin(), pairVector.end(), make_pair(second_state->id, first_state->id)) != pairVector.end();
-    if(((c1 && !c2) || (!c1 && c2)) || (!c3 || !c4)){
-        if((c1 && !c2) || (!c1 && c2)) distingishable = true;
+void dfs_recursivo(Automata a, State* first_state, State* second_state, vector<pair<int,int>>& pairVector, vector<pair<int,int>>& visited, bool& distingishable){
+    if(distingishable){
         return;
     }
+    bool c1 = find(a.finalStates.begin(), a.finalStates.end(), first_state->id) == a.finalStates.end();
+    bool c2 = find(a.finalStates.begin(), a.finalStates.end(), second_state->id) == a.finalStates.end();
+    bool c3 = find(visited.begin(), visited.end(), make_pair(first_state->id, second_state->id)) != visited.end();
+    bool c4 = find(visited.begin(), visited.end(), make_pair(second_state->id, first_state->id)) != visited.end();
+    bool c5 = (first_state->id==second_state->id);
+    if(((c1 && !c2) || (!c1 && c2)) || (c3 || c4) || c5){
+        if((c1 && !c2) || (!c1 && c2)){
+            distingishable = true;
+            pairVector.emplace_back(first_state->id, second_state->id);
+        }
+        else{
+            pairVector.erase(pairVector.begin()+1, pairVector.end());
+        }
+        return;
+    }
+    visited.emplace_back(first_state->id, second_state->id);
     pairVector.emplace_back(first_state->id, second_state->id);
     Transition* zero_first_transition;
     Transition* one_first_transition;
@@ -105,19 +90,19 @@ void dfs_recursivo(Automata a, State* first_state, State* second_state, vector<p
             break;
         default: throw out_of_range("ERROR: El Automata no es un AFD");
     }
-    transition = second_state->transitions[1];
+    transition = second_state->transitions[0];
     switch(transition->data) {
         case 0:
             zero_second_transition = transition;
-            one_second_transition = first_state->transitions[1];
+            one_second_transition = second_state->transitions[1];
             break;
         case 1:
             one_second_transition = transition;
-            zero_second_transition = first_state->transitions[1];
+            zero_second_transition = second_state->transitions[1];
             break;
         default: throw out_of_range("ERROR: El Automata no es un AFD");
     }
-    dfs_recursivo(a, zero_first_transition->states[1], zero_second_transition->states[1], pairVector, distingishable);
-    dfs_recursivo(a, one_first_transition->states[1], one_second_transition->states[1], pairVector, distingishable);
+    dfs_recursivo(a, zero_first_transition->states[1], zero_second_transition->states[1], pairVector, visited, distingishable);
+    dfs_recursivo(a, one_first_transition->states[1], one_second_transition->states[1], pairVector, visited, distingishable);
 }
 #endif //TEO_PROYECTO_BETTERMATRIX_H
