4a641eb688481314e0a4db977a2b6fc7ad2e193d
Efraín Córdova <52300931+ecordova0975@users.noreply.github.com>
2020-12-16 23:06:06 -0500

    main.cpp edited

--- main.cpp
+++ main.cpp
@@ -4,21 +4,46 @@
 #include "revautomata.h"
 #include "equivalencia.h"
 #include "brozozowski.h"
+#include <ctime>
+#include <iomanip>
 
 int main() {
+    unsigned t0, t1;
     auto a = Automata();
     a.readInput();
-    auto xd = equivalencia_optimizada(a);
-    auto porfavor = HuffMore(a);
-    porfavor.display();
-    //printMatrizEquivalencia(xd);
-    //auto temp = singleDFS(a);
-    //for(int i = 0; i < a.states.size(); i++) cout<<temp[i]<<" ";
-    //a.display();
-/*    auto b = Brzozowski(a);
-    b.display();*/
-    cout<<endl;
-    //cout<<"-------------------------------"<<endl;
-    //printMatrizEquivalencia(equivalenciaEstados(a));
+
+    t0 = clock();
+    cout<<"  1 - Brzozowski: "<<endl;
+    //auto Brozozowski = Brzozowski(a);t1 = clock();
+    //Brozozowski.display(); cout<<endl;
+    //double time = (double(t1-t0)/CLOCKS_PER_SEC);
+    //cout << "Execution Time: " << time << endl << endl;
+
+
+    t0 = clock();
+    cout<<"  2 - Matriz de Equivalencia (O(n^4)): "<<endl;
+    auto Matriz_equivalencia = equivalenciaEstados(a);
+    t1 = clock();
+    printMatrizEquivalencia(Matriz_equivalencia); cout<<endl;
+/*    double time = (double(t1-t0)/CLOCKS_PER_SEC);
+    cout << "Execution Time: " << std::setprecision(8) << time << endl << endl;*/
+
+    t0 = clock();
+    cout<<"  3 - Matriz de Equivalencia (O(n^2)): "<<endl;
+    auto Matriz_equivalencia_mejorada = equivalencia_optimizada(a);
+    t1 = clock();
+    printMatrizEquivalencia(Matriz_equivalencia_mejorada); cout<<endl;
+/*    time = (double(t1-t0)/CLOCKS_PER_SEC);
+    cout << "Execution Time: " << std::setprecision(8) << time << endl << endl;*/
+
+    t0 = clock();
+    cout<<"  4 - Huffman-Moore: "<<endl;
+    auto Huffman_Moore = HuffMore(a);
+    t1 = clock();
+    Huffman_Moore.display(); cout<<endl;
+/*    time = (double(t1-t0)/CLOCKS_PER_SEC);
+    cout << "Execution Time: " << std::setprecision(8) << time << endl << endl;
+    cout<<endl;*/
+
     return 0;
 }
